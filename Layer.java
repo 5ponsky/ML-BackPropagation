393edab895529800f7f83c84902bad066bf86a12
5ponsky <mailgd9@gmail.com>
2018-02-04 14:21:44 -0600

    template

--- Layer.java
+++ Layer.java
@@ -1,14 +1,20 @@
 abstract class Layer
 {
 	protected Vec activation;
+	protected Vec blame;
 	protected int inputs, outputs;
 
 	Layer(int inputs, int outputs)
 	{
 		activation = new Vec(outputs);
+		blame = new Vec(outputs);
 		this.inputs = inputs;
 		this.outputs = outputs;
 	}
 
 	abstract void activate(Vec weights, Vec x);
+
+	abstract void backprop(Vec weights, Vec prevBlame);
+
+	abstract void updateGradient(Vec x, Vec gradient);
 }
